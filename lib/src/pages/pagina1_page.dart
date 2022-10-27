511a052a0cb61d045be3e065e6973914098664ff
ddienu <rockdann@hotmail.com>
2022-10-27 00:41:57 -0500

    Add: pagina1, update: main
    Add pagina1 page into the folder pages, it returns a scaffold to prepare the page to start code.
    Update main implementing the pagina1 page into the home of the material app

---
+++ lib/src/pages/pagina1_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class Pagina1Page extends StatelessWidget {
+  const Pagina1Page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: Center(
+        child: Text('Hola Mundo'),
+      ),
+    );
+    
+  }
+}
