84fff883b375596008992fc991a4c9de780056bf
ddienu <rockdann@hotmail.com>
2022-10-27 01:18:37 -0500

    Update: pagina1 page, update: pubspec
    Update pagina1 page implementing an appbar with two icon button as actions, also implementing the body of the scaffold with a column with an icon, two texts and a container as childrens of the column, and in the end implementing a floating action button.
    Update pubspec installing the font awesome package from the pub.dev

--- lib/src/pages/pagina1_page.dart
+++ lib/src/pages/pagina1_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 
 class Pagina1Page extends StatelessWidget {
   const Pagina1Page({Key? key}) : super(key: key);
@@ -8,9 +10,53 @@ class Pagina1Page extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
+      appBar: AppBar(
+        title: Text('Animate_do'),
+        actions: [
+
+          IconButton(
+            onPressed: (){},
+            icon: FaIcon(FontAwesomeIcons.twitter)
+            ),
+
+          IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.navigate_next)
+            ),
+        ],
       ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            Icon(Icons.new_releases, color: Colors.blue, size: 40.0),
+
+            Text('Título', style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.w200)),
+
+            Text(' Título pequeño', 
+              style: TextStyle( 
+                fontSize: 20.0, 
+                fontWeight: FontWeight.w400, 
+                  )
+            ),
+
+            Container(
+              width: 220.0,
+              height: 2,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            )
+          ],
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: FaIcon( FontAwesomeIcons.play ),
+        ),
     );
     
   }
