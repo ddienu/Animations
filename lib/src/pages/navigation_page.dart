eb42608a322cec2cfeb42d2ff61ed15eec14a9d8
ddienu <rockdann@hotmail.com>
2022-11-01 23:58:21 -0500

    Update: navigation page.
    Update navigation page implementing an animation to notifications icon, it consist in when the notifications number is 0 the animation is with opacity 0 and when the notification number is 1, the opacity of the animation is 1 to make visible the animation to the user,

--- lib/src/pages/navigation_page.dart
+++ lib/src/pages/navigation_page.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
@@ -35,12 +36,23 @@ class _BotonFlotante extends StatelessWidget {
   Widget build(BuildContext context) {
 
     int numero = Provider.of<_NotificationModel>(context).numero;
+
+    final notiModel = Provider.of<_NotificationModel>(context, listen: false);
     
     return FloatingActionButton(
       onPressed: (){
-        Provider.of<_NotificationModel>(context, listen: false).numero;
+        notiModel.numero;
         numero++;
-        Provider.of<_NotificationModel>(context, listen: false).numero = numero;
+        notiModel.numero = numero;
+
+
+        if ( numero >= 2 ){
+
+          final controller = notiModel.bounceController;
+          
+          controller.forward(from: 0.0);
+
+        }
 
       },
       child: FaIcon( FontAwesomeIcons.play),
@@ -78,14 +90,22 @@ class _BottomNavigation extends StatelessWidget {
               Positioned(
                 top: 0.0,
                 right: 0.0,
-                child: Container(
-                  child: Text( '$numero', style: TextStyle( color: Colors.white, fontSize: 8, fontWeight: FontWeight.bold),),
-                  width: 12,
-                  height: 12,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.redAccent,
-                    shape: BoxShape.circle,
+                child: BounceInDown(
+                  from: 12.0,
+                  animate: ( numero >= 1) ? true : false,
+                  child: Bounce(
+                    from: 10.0,
+                    controller: (controller) => Provider.of<_NotificationModel>(context).bounceController = controller,
+                      child: Container(
+                      child: Text( '$numero', style: TextStyle( color: Colors.white, fontSize: 8, fontWeight: FontWeight.bold),),
+                        width: 12,
+                        height: 12,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: Colors.redAccent,
+                          shape: BoxShape.circle,
+                      ),
+                    ),
                   ),
                 ),
               )
@@ -107,11 +127,20 @@ class _NotificationModel extends ChangeNotifier {
 
   int _numero = 0;
 
-  int get numero => this._numero;
+  late AnimationController _bounceController;
+
+  int get numero => _numero;
 
   set numero ( int valor){
     _numero = valor;
     notifyListeners();
+  }
+
+  AnimationController get bounceController => _bounceController;
+
+  set bounceController ( AnimationController controller){
+    _bounceController = controller;
+    //notifyListeners();
   }
 
 }
